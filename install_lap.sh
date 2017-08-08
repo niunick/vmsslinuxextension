dd23b80d0cf611b813cd85253d4237fd21815f9b
niuyuxiao <niuyuxiao@139.com>
2017-08-08 15:22:49 +0800

    3

--- install_lap.sh
+++ install_lap.sh
@@ -3,13 +3,11 @@
 # install Apache and PHP (in a loop because a lot of installs happen
 # on VM init, so won't be able to grab the dpkg lock immediately)
 sudo su -
-until apt-get -y update && apt-get -y install apache2 php
-do
-  echo "Try again to install apache2 php" >> apt-get.out
-  sleep 2
-done
-echo "Succeed to install apache2 php" >>apt-get.out
-# write some PHP; these scripts are downloaded beforehand as fileUris
+apt-get -y update
+apt-get -y update
+apt-get -y install apache2 php5
+apt-get -y install apache2 php5
+
 cp index.php /var/www/html/
 cp do_work.php /var/www/html/
 rm /var/www/html/index.html
